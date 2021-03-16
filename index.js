3d9892ff0b5ef8ee05ceb6a113c8ab65b0bb9fc6
Hpollard003 <hpollard003@gmail.com>
2021-03-16 14:02:10 -0400

    commit tree 9a133852a13c05bfa5a6b54bf85c2a6348bb0aa8

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser ='Grace Hopper';
+//current user is Grace
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//if the welcome message is printed it will say "Welcome to Flatbook, Grace Hopper!"
+//`` is where I found some trouble but i caught it
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//Uppercase version
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+// the const is normal but with the template literal and slice we can pull the current users name and break it down to the first letter
