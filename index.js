9dbcf5f430123c6b482841a5c8712bfca848318c
Caner <kurucaner@gmail.com>
2021-08-09 12:40:48 -0400

    commit tree f582a0bb4e6f715be9add2c2f3c530b40de31eea

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = "Adalya";
+const welcomeMessage = `'Welcome to Flatbook, ' + ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
