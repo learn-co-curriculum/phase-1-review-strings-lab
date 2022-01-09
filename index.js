393c8fbf2ab9d5c858d12c7a4b9e0e0a6aaeaaca
Kirstie <kirstiewithrow@gmail.com>
2022-01-09 04:58:32 -0600

    commit tree 95b70d51d5b4f324ba351e447696025faa48a6d7

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// Write your code in this file!
-const currentUser = 'Grace Hopper';
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const currentUser = 'Kirstie Withrow'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
