317892dac24050423f1c8f163f93c02f1973c235
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-01-11 21:44:04 -0600

    commit tree 3c709208e265265010b9a160583b238607579570

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "Jacey";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
+
+console.log(shortGreeting);
