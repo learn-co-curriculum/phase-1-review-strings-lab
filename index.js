84b1d8dd96b7fdc89364b287d58b85f852b52577
Ibaihima <ibaihima@hotmail.com>
2022-01-30 23:50:17 -0500

    commit tree f6768d288d755d490865d5360cf2ef060a8c3fc6

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "iba"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
