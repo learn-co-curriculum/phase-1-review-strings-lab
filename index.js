87c1e1cb1739ef75fb8eba5daf29b235fc37c1b8
Benjamin Shutyak <bshutyak@gmail.com>
2022-03-04 10:12:45 -0700

    commit tree a2da185a9a7748be533564e6d242154bf0fbe582

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
