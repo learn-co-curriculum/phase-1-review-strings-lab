462f451706ebfd6c907cffc0a900822aeacf1bae
Daniel Zhang <dzhangwh@gmail.com>
2022-12-01 21:44:38 -0500

    commit tree 747b5de83eafccf9245113f3a64105461190d71b

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const currentUser = 'Grace Hopper';
+/*const currentUser = 'Grace Hopper';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 const shortGreeting = `Welcome, ${currentUser.slice(0,1)} !`;
+*/
+
+//quick syntax review from memory
+
+
+var currentUser = 'Grace Hopper';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//var shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+var secondUser = `easdsg`;
+
+var capSecondUser = secondUser.toUpperCase();
+
+var shortGreeting = `Welcome, ${capSecondUser.slice(5, 6)}!`
